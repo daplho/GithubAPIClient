9efd6c3591ff757302f94b509dbf2a3827ca6fd1
daplho <nskirov@gmail.com>
2022-05-17 11:22:56 -0700

    * fixed some "nullable" errors

--- Repository.cs
+++ Repository.cs
@@ -3,18 +3,18 @@ using System.Text.Json.Serialization;
 namespace GithubAPIClient {
     public class Repository {
         [JsonPropertyName("description")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         [JsonPropertyName("html_url")]
-        public Uri GithubHomeUrl { get; set; }
+        public Uri? GithubHomeUrl { get; set; }
 
         [JsonPropertyName("homepage")]
-        public Uri Homepage { get; set; }
+        public Uri? Homepage { get; set; }
 
         [JsonPropertyName("name")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        [JsonPropertyName("pushed_at")]
+        [JsonPropertyName("pushed_at")] 
         public DateTime LastPushUtc { get; set; }
 
         public DateTime LastPush => LastPushUtc.ToLocalTime();
